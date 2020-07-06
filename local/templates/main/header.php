150cbc3be6425de01fbb5db7d45ccb29ec5bf488
cesnakas <10352420+cesnakas@users.noreply.github.com>
2020-07-06 11:36:47 +0400

    1.0.6

--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -5,21 +5,33 @@
 <head>
 
 <?
-  $APPLICATION->ShowHead();
-  // Bitrix
-  use Bitrix\Main\Page\Asset;
-  // Meta
-  Asset::getInstance()->addString('<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover, user-scalable=0">');
-  // CSS
-  Asset::getInstance()->addCss(SITE_TEMPLATE_PATH.'/css/main.min.css');
-  // JS
-  Asset::getInstance()->addJs(SITE_TEMPLATE_PATH.'/js/main.js');
+    $APPLICATION->ShowHead();
+    // Bitrix
+    use Bitrix\Main\Page\Asset;
+    // Meta
+    Asset::getInstance()->addString('<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover, user-scalable=0">');
+    // CSS
+    Asset::getInstance()->addCss('https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.min.css');
+    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH.'/css/main.min.css');
+    // JS
+    Asset::getInstance()->addJs('https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js');
+    Asset::getInstance()->addJs('https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js');
+    Asset::getInstance()->addJs('https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/js/bootstrap.min.js');
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH.'/js/main.js');
 ?>
 
-  <title><?$APPLICATION->ShowTitle();?></title>
+    <title><?$APPLICATION->ShowTitle();?></title>
 
-  <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
 
 </head>
 <div id="panel"><?$APPLICATION->ShowPanel();?></div>
 <body>
+
+    <nav  role="navigation">
+        <div class="container">
+
+        </div>
+    </nav>
+
+    <main role="main">
