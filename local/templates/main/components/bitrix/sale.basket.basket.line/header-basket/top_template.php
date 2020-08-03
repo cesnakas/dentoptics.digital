479d356f70776138158a1fadeb9eec8ae4a09e84
cesnakas <10352420+cesnakas@users.noreply.github.com>
2020-08-03 14:03:48 +0400

    1.3.12

---
+++ local/templates/main/components/bitrix/sale.basket.basket.line/header-basket/top_template.php
@@ -0,0 +1,44 @@
+<?if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
+/**
+ * @global array $arParams
+ * @global CUser $USER
+ * @global CMain $APPLICATION
+ * @global string $cartId
+ */
+$compositeStub = (isset($arResult['COMPOSITE_STUB']) && $arResult['COMPOSITE_STUB'] == 'Y');
+?>
+
+<?if (!$arResult["DISABLE_USE_BASKET"]) { ?>
+    <a href="<?=$arParams['PATH_TO_BASKET']?>" style="position: relative;">
+        <img src="<?=SITE_TEMPLATE_PATH;?>/img/svg/basket.svg" alt="">
+        <span class="sr-only"><?=GetMessage('TSB1_CART')?></span>
+
+<? } ?>
+
+<?
+if (!$compositeStub) {
+    if ($arParams['SHOW_NUM_PRODUCTS'] == 'Y' && ($arResult['NUM_PRODUCTS'] > 0 || $arParams['SHOW_EMPTY_VALUES'] == 'Y')) {
+?>
+        <sup style="position: absolute; top: 4px;">
+            <?/*=$arResult['BASKET_COUNT_DESCRIPTION'];*/?>
+            <?=$arResult['NUM_PRODUCTS']?>
+        </sup>
+    </a>
+<?
+        if ($arParams['SHOW_TOTAL_PRICE'] == 'Y') {
+            ?>
+            <br <? if ($arParams['POSITION_FIXED'] == 'Y'): ?>class="d-none d-block-sm"<? endif; ?>/>
+            <span>
+                <?=GetMessage('TSB1_TOTAL_PRICE')?> <strong><?=$arResult['TOTAL_PRICE']?></strong>
+            </span>
+            <?
+        }
+    }
+}
+?>
+
+<? if ($arParams['SHOW_PERSONAL_LINK'] == 'Y'):?>
+    <div class="mt-1 basket-line-block">
+        <a href="<?=$arParams['PATH_TO_PERSONAL']?>"><?=GetMessage('TSB1_PERSONAL')?></a>
+    </div>
+<? endif ?>
--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -117,20 +117,35 @@
                     </div>
                 </div>
                 <div class="col-2 d-flex justify-content-end">
-                    <div class="header_basket">
-                        <a href="#">
-                            <img src="<?=SITE_TEMPLATE_PATH;?>/img/svg/compare.svg" alt="">
-                            <span class="sr-only">Compare</span>
-                        </a>
-                        <a href="#">
-                            <img src="<?=SITE_TEMPLATE_PATH;?>/img/svg/subscribe.svg" alt="">
-                            <span class="sr-only">Subscribe</span>
-                        </a>
-                        <a href="#">
-                            <img src="<?=SITE_TEMPLATE_PATH;?>/img/svg/basket.svg" alt="">
-                            <span class="sr-only">Basket</span>
-                        </a>
-                    </div>
+
+                    <?$APPLICATION->IncludeComponent(
+                        "bitrix:sale.basket.basket.line",
+                        "header-basket",
+                        array(
+                            "HIDE_ON_BASKET_PAGES" => "N",
+                            "PATH_TO_PERSONAL" => SITE_DIR."personal/",
+                            "PATH_TO_PROFILE" => SITE_DIR."personal/",
+                            "PATH_TO_REGISTER" => SITE_DIR."login/",
+                            "SHOW_AUTHOR" => "Y",
+                            "SHOW_DELAY" => "Y",
+                            "SHOW_EMPTY_VALUES" => "Y",
+                            "SHOW_IMAGE" => "N",
+                            "SHOW_NOTAVAIL" => "Y",
+                            "SHOW_PERSONAL_LINK" => "N",
+                            "SHOW_PRICE" => "N",
+                            "SHOW_REGISTRATION" => "N",
+                            "SHOW_TOTAL_PRICE" => "N",
+                            "COMPONENT_TEMPLATE" => "header-basket",
+                            "PATH_TO_BASKET" => SITE_DIR."personal/cart/",
+                            "PATH_TO_ORDER" => SITE_DIR."personal/order/make/",
+                            "SHOW_NUM_PRODUCTS" => "Y",
+                            "PATH_TO_AUTHORIZE" => "",
+                            "SHOW_PRODUCTS" => "N",
+                            "POSITION_FIXED" => "N"
+                        ),
+                        false
+                    );?>
+
                 </div>
             </div>
         </div>
