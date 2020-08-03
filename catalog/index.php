bce5ade3949c2beb0e2d5c3e7b370486347de0bf
cesnakas <10352420+cesnakas@users.noreply.github.com>
2020-08-03 15:06:15 +0400

    1.3.14

--- catalog/index.php
+++ catalog/index.php
@@ -3,7 +3,12 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Каталог продукции");
 ?>
 
-Text here....
+
+    <section>
+        <div class="container-xl">
+            Text here....
+        </div>
+    </section>
 
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
