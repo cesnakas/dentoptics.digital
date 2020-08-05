728a9b5b40c222a59969f4ab6ac2282964009466
cesnakas <10352420+cesnakas@users.noreply.github.com>
2020-08-05 14:27:20 +0400

    1.3.20

--- local/dev/index.php
+++ local/dev/index.php
@@ -1,8 +1,41 @@
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+$APPLICATION->SetTitle("");
 ?>
 
+<section>
+	<div class="container">
 
+        <?$APPLICATION->IncludeComponent(
+            "bitrix:sale.basket.basket.line",
+            "header-basket",
+            array(
+                "HIDE_ON_BASKET_PAGES" => "N",
+                "PATH_TO_PERSONAL" => SITE_DIR."personal/",
+                "PATH_TO_PROFILE" => SITE_DIR."personal/",
+                "PATH_TO_REGISTER" => SITE_DIR."login/",
+                "SHOW_AUTHOR" => "Y",
+                "SHOW_DELAY" => "Y",
+                "SHOW_EMPTY_VALUES" => "Y",
+                "SHOW_IMAGE" => "N",
+                "SHOW_NOTAVAIL" => "Y",
+                "SHOW_PERSONAL_LINK" => "N",
+                "SHOW_PRICE" => "N",
+                "SHOW_REGISTRATION" => "N",
+                "SHOW_TOTAL_PRICE" => "N",
+                "COMPONENT_TEMPLATE" => "header-basket",
+                "PATH_TO_BASKET" => SITE_DIR."personal/cart/",
+                "PATH_TO_ORDER" => SITE_DIR."personal/order/make/",
+                "SHOW_NUM_PRODUCTS" => "Y",
+                "PATH_TO_AUTHORIZE" => "",
+                "SHOW_PRODUCTS" => "N",
+                "POSITION_FIXED" => "N"
+            ),
+            false
+        );?>
+		
+	</div>
+</section>
 
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
--- local/templates/main/footer.php
+++ local/templates/main/footer.php
@@ -5,41 +5,97 @@
     <footer class="footer">
         <div class="container-xl">
             <div class="row">
-                <div class="col-3">
-                    <p>БУДЕМ НА СВЯЗИ!</p>
-                    <p>Акции и бонусы для своих в один клик - и у вас в почте.</p>
-                    <input type="submit" value="">
-                    <p>Мы в социальных сетях</p>
-                    <div class="">
-
+                <div class="footer__left col-3">
+                    <p class="p">БУДЕМ НА СВЯЗИ!</p>
+                    <p class="p mb-4">
+                        Акции и бонусы для своих
+                        <br>
+                        в один клик - и у вас в почте.
+                    </p>
+                    <input type="email" value="" placeholder="Укажите Ваш E-mail">
+                    <div class="footer__left-social">
+                        <p class="">Мы в социальных сетях</p>
+                        <a href="#">
+                            <img src="/local/templates/main/img/svg/instagram.svg" alt="">
+                        </a>
+                        <a href="#">
+                            <img src="/local/templates/main/img/svg/vk.svg" alt="">
+                        </a>
+                        <a href="#">
+                            <img src="/local/templates/main/img/svg/fb.svg" alt="">
+                        </a>
                     </div>
                 </div>
-                <div class="col-6">
+                <div class="footer__center col-6">
                     <div class="row">
                         <div class="col-4">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti veniam doloremque fugiat repellat aliquid, nesciunt vero recusandae, iusto dolor quas, facere quibusdam reiciendis maiores. Tempora saepe est et rerum odio?
+                            <ul class="list-unstyled">
+                                <li><a href="#">Покупателям</a></li>
+                                <li><a href="#">Контакты</a></li>
+                                <li><a href="#">Обратная связь</a></li>
+                                <li><a href="#">Компания</a></li>
+                                <li><a href="#">Доставка и оплата</a></li>
+                            </ul>
                         </div>
                         <div class="col-4">
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde eum sunt in obcaecati a voluptates possimus nemo at adipisci soluta! Unde optio distinctio provident illum laudantium magnam eos assumenda soluta.
+                            <ul class="list-unstyled">
+                                <li><a href="#">CAT/СAM системы</a></li>
+                                <li><a href="#">Сканеры интраоральные</a></li>
+                                <li><a href="#">Сканеры лабораторные</a></li>
+                                <li><a href="#">Фрезы</a></li>
+                                <li><a href="#">Комплекты CAT/CAM</a></li>
+                            </ul>
                         </div>
                         <div class="col-4">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid inventore eius voluptas exercitationem, commodi, repellat hic modi unde et culpa non aspernatur amet libero earum, animi iste suscipit nisi minima!
+                            <ul class="list-unstyled">
+                                <li><a href="#">Расходные материалы</a></li>
+                                <li><a href="#">Диски</a></li>
+                                <li><a href="#">Блоки</a></li>
+                                <li><a href="#">Фрезы</a></li>
+                                <li><a href="#">3D печать</a></li>
+                                <li><a href="#">Аксессуары</a></li>
+                                <li><a href="#">Красители</a></li>
+                                <li><a href="#">Программное&nbsp;обеспечение</a></li>
+                            </ul>
                         </div>
                     </div>
                 </div>
-                <div class="col-3">
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, veniam?</p>
-                    <p>Lorem.</p>
-                    <button>Lorem, ipsum.</button>
-                    <div>
-
+                <div class="footer__right col-3">
+                    <div class="footer__right-info">
+                        <div>
+                            <a href="tel:">8 (495) 796 24 10</a>
+                            - с 10:00 до 20:00 часов по будням
+                        </div>
+                        <div>
+                            <a href="tel:">8 (800) 100 23 83</a>
+                            - горячая линия c 8:00 до 17:00
+                        </div>
+                    </div>
+                    <div class="footer__right-mail">
+                        <a href="mailto:">
+                            <img src="/local/templates/main/img/svg/mail-light.svg" alt="Mail">
+                            <span>mail@dentoptics.ru</span>
+                        </a>
+                    </div>
+                    <button class="btn btn-feedback">Заказать обратный звонок</button>
+                    <div class="footer__right-cards">
+                        <img src="/local/templates/main/img/svg/mastercard.svg" alt="MasterCard">
+                        <img src="/local/templates/main/img/svg/visa.svg" alt="VISA">
                     </div>
                 </div>
             </div>
         </div>
-        <div class="footer_rights">
+        <div class="footer__rights">
             <div class="container-xl">
-
+                <div class="row">
+                    <div class="col-2">© DENTOPTICS, 2020</div>
+                    <div class="col-3">
+                        <a href="#">Пользовательское соглашение и условия</a>
+                    </div>
+                    <div class="col-7">
+                        <a href="#">Обработка персональных данных</a>
+                    </div>
+                </div>
             </div>
         </div>
     </footer>
