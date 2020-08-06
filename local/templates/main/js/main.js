1c8ad13bdadffaee35bcb2ca332be29e425ee0d2
cesnakas <10352420+cesnakas@users.noreply.github.com>
2020-08-06 14:19:42 +0400

    1.3.22

--- local/templates/main/js/main.js
+++ local/templates/main/js/main.js
@@ -3,3 +3,21 @@ if ($('#bx-panel').length) {
 	$('#global_nav').css({'top':'auto','z-index':'99'});
 	// $('#catalog-nav').css('z-index','90');
 };
+
+// Slider
+var swiper = new Swiper('.slider .swiper-container', {
+	slidesPerView: 2,
+	spaceBetween: 30,
+	loop: true,
+	// effect: 'coverflow',
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	mousewheel: true,
+	keyboard: true,
+});
