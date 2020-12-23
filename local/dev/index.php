d695ed4349f2e909b3507c6a4e711f606360af9e
cesnakas <10352420+cesnakas@users.noreply.github.com>
2020-12-23 13:20:03 +0400

    1.16.121

--- local/dev/index.php
+++ local/dev/index.php
@@ -6,26 +6,7 @@ $APPLICATION->SetTitle("");
 <section class="section">
 	<div class="container-xl">
 
-        <?$APPLICATION->IncludeComponent(
-            "bitrix:catalog.compare.list",
-            "bootstrap_v4",
-            Array(
-                "ACTION_VARIABLE" => "action",
-                "AJAX_MODE" => "Y",
-                "AJAX_OPTION_ADDITIONAL" => "",
-                "AJAX_OPTION_HISTORY" => "Y",
-                "AJAX_OPTION_JUMP" => "N",
-                "AJAX_OPTION_STYLE" => "Y",
-                "COMPARE_URL" => SITE_DIR.'compare/',
-                "DETAIL_URL" => "/catalog/#SECTION_CODE#/#ELEMENT_CODE#/",
-                "IBLOCK_ID" => "1",
-                "IBLOCK_TYPE" => "catalog",
-                "NAME" => "CATALOG_COMPARE_LIST",
-                "POSITION" => "bottom right",
-                "POSITION_FIXED" => "Y",
-                "PRODUCT_ID_VARIABLE" => "id"
-            )
-        );?>
+
 		
 	</div>
 </section>
